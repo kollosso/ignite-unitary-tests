fa4c7c86c99c1f0ae77482a8cc74dfcb72743cce
denismedeirossdk <contato.denismedeiros@hotmail.com>
2021-04-01 11:56:06 -0300

    feat: create integ-test create user

---
+++ src/__test__/integration/modules/users/CreateUserController.spec.ts
@@ -0,0 +1,29 @@
+import request from 'supertest'
+import { Connection } from 'typeorm'
+
+import { app } from '../../../../app'
+
+import createConnection from '../../../../database'
+
+let connection: Connection
+
+describe('Create User Controller', () => {
+  beforeAll(async () => {
+    connection = await createConnection()
+
+    await connection.runMigrations()
+  })
+  afterAll(async () => {
+    await connection.dropDatabase()
+    await connection.close()
+  })
+  it('should be able to create a new user', async () => {
+    const response = await request(app).post('/api/v1/users').send({
+      name: "John Tree",
+      email: "johntree@mail.com",
+      password: "123456"
+    })
+
+    expect(response.status).toBe(201)
+  })
+})
